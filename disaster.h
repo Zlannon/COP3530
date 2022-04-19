8a69fa933f36b0ae3dcfe4b7c63bdbd40934b314
SolaceF2 <100873509+SolaceF2@users.noreply.github.com>
2022-04-19 15:19:50 -0400

    Update disaster.h

--- disaster.h
+++ disaster.h
@@ -14,17 +14,19 @@ struct disaster {
 	std::string month;
 	int year;
 	int damage;
+	std::string county;
 
 	disaster() {
 
 	}
 
-	disaster(std::string Type, std::string State, std::string Month, int Year, int Damage) {
+	disaster(std::string Type, std::string State, std::string Month, int Year, int Damage, std::string County) {
 		type = Type;
 		state = State;
 		month = Month;
 		year = Year;
 		damage = Damage;
+		county = County;
 	}
 
 	void Load(std::vector<disaster*>& Disasters, const std::string File, std::string country);
